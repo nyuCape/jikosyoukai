318ec883a30bc92e125766ea533e3e32154eb0e0
nyuCape <tomoki_20n1100573@nnn.ed.jp>
2021-02-01 01:16:23 +0900

    変更するよ
    ファイルを追加するよ

---
+++ assessment.js
@@ -0,0 +1,82 @@
+'use strict';
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+assessmentButton.onclick = () => {
+  const userName = userNameInput.value;
+  if (userName.length === 0) {
+    return;
+  }
+
+  removeAllChildren(resultDivided);
+  const header = document.createElement('h3');
+  header.innerText = '診断結果';
+  resultDivided.appendChild(header);
+
+  const paragraph = document.createElement('p');
+  const result = assessment(userName);
+  paragraph.innerText = result;
+  resultDivided.appendChild(paragraph);
+
+  // ツイートエリアの作成
+  removeAllChildren(tweetDivided);
+  const anchor = document.createElement('a');
+  const hrefValue =
+    'https://twitter.com/intent/tweet?button_hashtag=' +
+    encodeURIComponent('あなたのいいところ') +
+    '&ref_src=twsrc%5Etfw';
+  anchor.setAttribute('href', hrefValue);
+  anchor.className = 'twitter-hashtag-button';
+  anchor.setAttribute('data-text', result);
+  anchor.innerText = 'Tweet @tonxkina#あなたのいいところ';
+  tweetDivided.appendChild(anchor);
+
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+  tweetDivided.appendChild(script);
+};
+
+const answers = [
+    '{userName}のいいところは小指の第二関節です。{userName}の特徴的な関節の曲がり具合は皆を惹きつけ、心に残ります。',
+    '{userName}のいいところは積極性です。{userName}はこの診断を行ったことからも、なんでもやってみる気概が伺えます。',
+    '{userName}のいいところはクリックする力です。{userName}は力強く、しかししなやかなクリックで診断ボタンを押しました、そのクリックは芸術の域にまで達しています。',
+    '{userName}のいいところはセンスです。{userName}のセンスは数ある診断の中からこの診断を選んだことからもわかる通り光るものがあります。',
+    '{userName}のいいところは人望です。{userName}の人望はあなたの後ろにいる人のからの熱烈な眼差しからわかる通り光るものがあります。'
+];
+
+/**
+ * 名前の文字列を渡すと診断結果を返す関数
+ * @param {string} userName ユーザーの名前
+ * @return {string} 診断結果
+ */
+function assessment(userName) {
+  let sumOfCharCode = 0;
+  for (let i = 0; i < userName.length; i++) {
+    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+  }
+
+  const index = sumOfCharCode % answers.length;
+  let result = answers[index];
+
+  result = result.replace(/{userName}/g, userName);
+  return result;
+}
+
+// テストコード
+console.assert(
+  assessment('太郎') ===
+    '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+  '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+);
+console.assert(
+  assessment('太郎') === assessment('太郎'),
+  '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+);
